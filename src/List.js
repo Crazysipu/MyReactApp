7f8cc7d6330f320af992d73f0507030414161bfd
Harisankar Sahoo <96780240+Crazysipu@users.noreply.github.com>
2022-05-23 22:58:01 +0530

    Add files via upload

---
+++ src/List.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./List.css"
+ function List(props) {
+   const deleteItemFromList=key=>{
+     const newList=props.itemList.filter(itemObj=>{
+       return itemObj.key !==key;
+     })
+     props.updateItemList(newList)
+   }
+  return (
+    <div>
+      {props.itemList.map(itemObj =>{
+          return (
+          <div key={itemObj.key} className='item'>
+              <p>{itemObj.item}</p>
+              <button onClick={()=> deleteItemFromList(itemObj.key)}>Delete</button>
+              </div>)
+      })}
+    </div>
+  ) 
+  }
+export default List;
